3acb53233508ddddb51f44355a29bc6908ccf5c9
mahamjabbar456 <mahamjabbar320@gmail.com>
2024-03-21 22:09:24 +0500

    In this i solve famous_person and strip question

---
+++ main.js
@@ -0,0 +1,11 @@
+"use strict";
+// Question 4
+console.log('Alber Einstein once said,"A person who never made a mistake never tried anything new"');
+// Question 5
+let famous_person = "Alber Einstein";
+let message = famous_person + ' once said,"A person who never made a mistake never tried anything new"';
+console.log(message);
+// Question 6
+let person_name = "      Maham \t Jabbar           ";
+console.log(person_name);
+console.log(person_name.trim());
---
+++ main.ts
@@ -0,0 +1,12 @@
+// Question 4
+console.log('Alber Einstein once said,"A person who never made a mistake never tried anything new"');
+
+// Question 5
+let famous_person:string = "Alber Einstein";
+let message:string = famous_person + ' once said,"A person who never made a mistake never tried anything new"';
+console.log(message);
+
+// Question 6
+let person_name:string = "      Maham \t Jabbar           ";
+console.log(person_name);
+console.log(person_name.trim());
